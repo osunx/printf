e9cde8a0d8748f4786082ef19b477ac7a67cdb9a
osunx <splendorfreeman1@gmail.com>
2023-07-23 13:43:29 -0700

    Print function C

--- _printf.c
+++ _printf.c
@@ -1,28 +1,67 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
-* customPrint - Prints formatted output to stdout using
-* a variable argument list.
-*
-* @format: A string containing the format specifier.
-* The format string is composed of zero or more directives.
-* See man 3 printf for more details on the format specifiers.
-* @...: Additional arguments that correspond to the format
-* specifiers in the format string.
-*
-* Description:
-* This function is a custom implementation of the printf function.
-* It takes a format string and variable arguments and prints the
-* formatted output to stdout.
-* The function uses the vprintf function from the standard C
-* library to handle the variable arguments.
-*
-* Return: None (void)
-*/
-void customPrint(const char *format, ...)
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
-vprintf(format, args);
-va_end(args);
+int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+va_list list;
+char buffer[BUFF_SIZE];
+
+if (format == NULL)
+return (-1);
+
+va_start(list, format);
+
+for (i = 0; format && format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+/* write(1, &format[i], 1);*/
+printed_chars++;
 }
+else
+{
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, list);
+precision = get_precision(format, &i, list);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+flags, width, precision, size);
+if (printed == -1)
+return (-1);
+printed_chars += printed;
+}
+}
+
+print_buffer(buffer, &buff_ind);
+
+va_end(list);
+
+return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+
+*buff_ind = 0;
+}
+/*Collaborators: Freeman, Nimrod Sekwati*/
